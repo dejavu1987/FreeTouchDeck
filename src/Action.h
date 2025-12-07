3a396ed7c98cedad61effc3ffb29c86af9b80400
Anil Maharjan <1720245+dejavu1987@users.noreply.github.com>
2025-12-07 09:56:56 +0100

    dry actions

--- src/Action.h
+++ src/Action.h
@@ -28,84 +28,49 @@ void bleKeyboardAction(int action, int value, char *symbol) {
     delay(value);
     break;
   case 2: // Send TAB ARROW etc
-    switch (value) {
-    case 1:
-      bleCombo.write(KEY_UP_ARROW);
-      break;
-    case 2:
-      bleCombo.write(KEY_DOWN_ARROW);
-      break;
-    case 3:
-      bleCombo.write(KEY_LEFT_ARROW);
-      break;
-    case 4:
-      bleCombo.write(KEY_RIGHT_ARROW);
-      break;
-    case 5:
-      bleCombo.write(KEY_BACKSPACE);
-      break;
-    case 6:
-      bleCombo.write(KEY_TAB);
-      break;
-    case 7:
-      bleCombo.write(KEY_RETURN);
-      break;
-    case 8:
-      bleCombo.write(KEY_PAGE_UP);
-      break;
-    case 9:
-      bleCombo.write(KEY_PAGE_DOWN);
-      break;
-    case 10:
-      bleCombo.write(KEY_DELETE);
-      break;
-    case 11:
-      bleCombo.write(KEY_PRTSC);
-      break;
-    case 12:
-      bleCombo.write(KEY_ESC);
-      break;
-    case 13:
-      bleCombo.write(KEY_HOME);
-      break;
-    case 14:
-      bleCombo.write(KEY_END);
-      break;
-
-    default:
-      // if nothing matches do nothing
-      break;
+    {
+      static const uint8_t keyMap[] = {
+        0,              // 0 - unused
+        KEY_UP_ARROW,   // 1
+        KEY_DOWN_ARROW, // 2
+        KEY_LEFT_ARROW, // 3
+        KEY_RIGHT_ARROW,// 4
+        KEY_BACKSPACE,  // 5
+        KEY_TAB,        // 6
+        KEY_RETURN,     // 7
+        KEY_PAGE_UP,    // 8
+        KEY_PAGE_DOWN,  // 9
+        KEY_DELETE,     // 10
+        KEY_PRTSC,      // 11
+        KEY_ESC,        // 12
+        KEY_HOME,       // 13
+        KEY_END         // 14
+      };
+      
+      if (value > 0 && value < sizeof(keyMap)/sizeof(keyMap[0])) {
+        bleCombo.write(keyMap[value]);
+      }
     }
     break;
   case 3: // Send Media Key
 #if defined(USEUSBHID)
 
 #else
-    switch (value) {
-    case 1:
-      bleCombo.write(KEY_MEDIA_MUTE);
-      break;
-    case 2:
-      bleCombo.write(KEY_MEDIA_VOLUME_DOWN);
-      break;
-    case 3:
-      bleCombo.write(KEY_MEDIA_VOLUME_UP);
-      break;
-    case 4:
-      bleCombo.write(KEY_MEDIA_PLAY_PAUSE);
-      break;
-    case 5:
-      bleCombo.write(KEY_MEDIA_STOP);
-      break;
-    case 6:
-      bleCombo.write(KEY_MEDIA_NEXT_TRACK);
-      break;
-    case 7:
-      bleCombo.write(KEY_MEDIA_PREVIOUS_TRACK);
-      break;
-    default:
-      // if nothing matches do nothing
-      break;
+    {
+      static const uint8_t mediaKeyMap[] = {
+        0,                           // 0 - unused
+        KEY_MEDIA_MUTE,              // 1
+        KEY_MEDIA_VOLUME_DOWN,       // 2
+        KEY_MEDIA_VOLUME_UP,         // 3
+        KEY_MEDIA_PLAY_PAUSE,        // 4
+        KEY_MEDIA_STOP,              // 5
+        KEY_MEDIA_NEXT_TRACK,        // 6
+        KEY_MEDIA_PREVIOUS_TRACK     // 7
+      };
+      
+      if (value > 0 && value < sizeof(mediaKeyMap)/sizeof(mediaKeyMap[0])) {
+        bleCombo.write(mediaKeyMap[value]);
+      }
     }
     break;
 #endif    // if defined(USEUSBHID)
@@ -113,116 +78,59 @@ void bleKeyboardAction(int action, int value, char *symbol) {
     bleCombo.print(symbol);
     break;
   case 5: // Option Keys
-    switch (value) {
-    case 1:
-      bleCombo.keyPress(KEY_LEFT_CTRL);
-      break;
-    case 2:
-      bleCombo.keyPress(KEY_LEFT_SHIFT);
-      break;
-    case 3:
-      bleCombo.keyPress(KEY_LEFT_ALT);
-      break;
-    case 4:
-      bleCombo.keyPress(KEY_LEFT_GUI);
-      break;
-    case 5:
-      bleCombo.keyPress(KEY_RIGHT_CTRL);
-      break;
-    case 6:
-      bleCombo.keyPress(KEY_RIGHT_SHIFT);
-      break;
-    case 7:
-      bleCombo.keyPress(KEY_RIGHT_ALT);
-      break;
-    case 8:
-      bleCombo.keyPress(KEY_RIGHT_GUI);
-      break;
-    case 9:
-      bleCombo.keyReleaseAll();
-      break;
-    default:
-      // if nothing matches do nothing
-      break;
+    {
+      static const uint8_t optionKeyMap[] = {
+        0,                // 0 - unused
+        KEY_LEFT_CTRL,    // 1
+        KEY_LEFT_SHIFT,   // 2
+        KEY_LEFT_ALT,     // 3
+        KEY_LEFT_GUI,     // 4
+        KEY_RIGHT_CTRL,   // 5
+        KEY_RIGHT_SHIFT,  // 6
+        KEY_RIGHT_ALT,    // 7
+        KEY_RIGHT_GUI     // 8
+      };
+      
+      if (value == 9) {
+        bleCombo.keyReleaseAll();
+      } else if (value > 0 && value < sizeof(optionKeyMap)/sizeof(optionKeyMap[0])) {
+        bleCombo.keyPress(optionKeyMap[value]);
+      }
     }
     break;
   case 6: // Function Keys
-    switch (value) {
-    case 1:
-      bleCombo.keyPress(KEY_F1);
-      break;
-    case 2:
-      bleCombo.keyPress(KEY_F2);
-      break;
-    case 3:
-      bleCombo.keyPress(KEY_F3);
-      break;
-    case 4:
-      bleCombo.keyPress(KEY_F4);
-      break;
-    case 5:
-      bleCombo.keyPress(KEY_F5);
-      break;
-    case 6:
-      bleCombo.keyPress(KEY_F6);
-      break;
-    case 7:
-      bleCombo.keyPress(KEY_F7);
-      break;
-    case 8:
-      bleCombo.keyPress(KEY_F8);
-      break;
-    case 9:
-      bleCombo.keyPress(KEY_F9);
-      break;
-    case 10:
-      bleCombo.keyPress(KEY_F10);
-      break;
-    case 11:
-      bleCombo.keyPress(KEY_F11);
-      break;
-    case 12:
-      bleCombo.keyPress(KEY_F12);
-      break;
-    case 13:
-      bleCombo.keyPress(KEY_F13);
-      break;
-    case 14:
-      bleCombo.keyPress(KEY_F14);
-      break;
-    case 15:
-      bleCombo.keyPress(KEY_F15);
-      break;
-    case 16:
-      bleCombo.keyPress(KEY_F16);
-      break;
-    case 17:
-      bleCombo.keyPress(KEY_F17);
-      break;
-    case 18:
-      bleCombo.keyPress(KEY_F18);
-      break;
-    case 19:
-      bleCombo.keyPress(KEY_F19);
-      break;
-    case 20:
-      bleCombo.keyPress(KEY_F20);
-      break;
-    case 21:
-      bleCombo.keyPress(KEY_F21);
-      break;
-    case 22:
-      bleCombo.keyPress(KEY_F22);
-      break;
-    case 23:
-      bleCombo.keyPress(KEY_F23);
-      break;
-    case 24:
-      bleCombo.keyPress(KEY_F24);
-      break;
-    default:
-      // if nothing matches do nothing
-      break;
+    {
+      static const uint8_t functionKeyMap[] = {
+        0,        // 0 - unused
+        KEY_F1,   // 1
+        KEY_F2,   // 2
+        KEY_F3,   // 3
+        KEY_F4,   // 4
+        KEY_F5,   // 5
+        KEY_F6,   // 6
+        KEY_F7,   // 7
+        KEY_F8,   // 8
+        KEY_F9,   // 9
+        KEY_F10,  // 10
+        KEY_F11,  // 11
+        KEY_F12,  // 12
+        KEY_F13,  // 13
+        KEY_F14,  // 14
+        KEY_F15,  // 15
+        KEY_F16,  // 16
+        KEY_F17,  // 17
+        KEY_F18,  // 18
+        KEY_F19,  // 19
+        KEY_F20,  // 20
+        KEY_F21,  // 21
+        KEY_F22,  // 22
+        KEY_F23,  // 23
+        KEY_F24   // 24
+      };
+      
+      if (value > 0 && value < sizeof(functionKeyMap)/sizeof(functionKeyMap[0])) {
+        bleCombo.keyPress(functionKeyMap[value]);
+      }
     }
     break;
   case 7: // Send Number
@@ -232,223 +140,68 @@ void bleKeyboardAction(int action, int value, char *symbol) {
     bleCombo.print(symbol);
     break;
   case 9: // Combos
-    switch (value) {
-    case 1:
-      bleCombo.keyPress(KEY_LEFT_CTRL);
-      bleCombo.keyPress(KEY_LEFT_SHIFT);
-      break;
-    case 2:
-      bleCombo.keyPress(KEY_LEFT_ALT);
-      bleCombo.keyPress(KEY_LEFT_SHIFT);
-      break;
-    case 3:
-      bleCombo.keyPress(KEY_LEFT_GUI);
-      bleCombo.keyPress(KEY_LEFT_SHIFT);
-      break;
-    case 4:
-      bleCombo.keyPress(KEY_LEFT_CTRL);
-      bleCombo.keyPress(KEY_LEFT_GUI);
-      break;
-    case 5:
-      bleCombo.keyPress(KEY_LEFT_ALT);
-      bleCombo.keyPress(KEY_LEFT_GUI);
-      break;
-    case 6:
-      bleCombo.keyPress(KEY_LEFT_CTRL);
-      bleCombo.keyPress(KEY_LEFT_ALT);
-      break;
-    case 7:
-      bleCombo.keyPress(KEY_LEFT_CTRL);
-      bleCombo.keyPress(KEY_LEFT_ALT);
-      bleCombo.keyPress(KEY_LEFT_GUI);
-      break;
-    case 8:
-      bleCombo.keyPress(KEY_RIGHT_CTRL);
-      bleCombo.keyPress(KEY_RIGHT_SHIFT);
-      break;
-    case 9:
-      bleCombo.keyPress(KEY_RIGHT_ALT);
-      bleCombo.keyPress(KEY_RIGHT_SHIFT);
-      break;
-    case 10:
-      bleCombo.keyPress(KEY_RIGHT_GUI);
-      bleCombo.keyPress(KEY_RIGHT_SHIFT);
-      break;
-    case 11:
-      bleCombo.keyPress(KEY_RIGHT_CTRL);
-      bleCombo.keyPress(KEY_RIGHT_GUI);
-      break;
-    case 12:
-      bleCombo.keyPress(KEY_RIGHT_ALT);
-      bleCombo.keyPress(KEY_RIGHT_GUI);
-      break;
-    case 13:
-      bleCombo.keyPress(KEY_RIGHT_CTRL);
-      bleCombo.keyPress(KEY_RIGHT_ALT);
-      break;
-    case 14:
-      bleCombo.keyPress(KEY_RIGHT_CTRL);
-      bleCombo.keyPress(KEY_RIGHT_ALT);
-      bleCombo.keyPress(KEY_RIGHT_GUI);
-      break;
+    {
+      static const uint8_t comboMap[][3] = {
+        {0, 0, 0},                                        // 0 - unused
+        {KEY_LEFT_CTRL, KEY_LEFT_SHIFT, 0},              // 1
+        {KEY_LEFT_ALT, KEY_LEFT_SHIFT, 0},               // 2  
+        {KEY_LEFT_GUI, KEY_LEFT_SHIFT, 0},               // 3
+        {KEY_LEFT_CTRL, KEY_LEFT_GUI, 0},                // 4
+        {KEY_LEFT_ALT, KEY_LEFT_GUI, 0},                 // 5
+        {KEY_LEFT_CTRL, KEY_LEFT_ALT, 0},                // 6
+        {KEY_LEFT_CTRL, KEY_LEFT_ALT, KEY_LEFT_GUI},     // 7
+        {KEY_RIGHT_CTRL, KEY_RIGHT_SHIFT, 0},            // 8
+        {KEY_RIGHT_ALT, KEY_RIGHT_SHIFT, 0},             // 9
+        {KEY_RIGHT_GUI, KEY_RIGHT_SHIFT, 0},             // 10
+        {KEY_RIGHT_CTRL, KEY_RIGHT_GUI, 0},              // 11
+        {KEY_RIGHT_ALT, KEY_RIGHT_GUI, 0},               // 12
+        {KEY_RIGHT_CTRL, KEY_RIGHT_ALT, 0},              // 13
+        {KEY_RIGHT_CTRL, KEY_RIGHT_ALT, KEY_RIGHT_GUI}   // 14
+      };
+      
+      if (value > 0 && value < sizeof(comboMap)/sizeof(comboMap[0])) {
+        for (int i = 0; i < 3; i++) {
+          if (comboMap[value][i] != 0) {
+            bleCombo.keyPress(comboMap[value][i]);
+          }
+        }
+      }
     }
     break;
   case 10: // Helpers
-    switch (value) {
-    case 1:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
+    {
+      static const uint8_t helperFunctionKeys[] = {
+        0,        // 0 - unused
+        KEY_F1,   // 1
+        KEY_F2,   // 2
+        KEY_F3,   // 3
+        KEY_F4,   // 4
+        KEY_F5,   // 5
+        KEY_F6,   // 6
+        KEY_F7,   // 7
+        KEY_F8,   // 8
+        KEY_F9,   // 9
+        KEY_F10,  // 10
+        KEY_F11   // 11
+      };
+      
+      if (value > 0 && value < sizeof(helperFunctionKeys)/sizeof(helperFunctionKeys[0])) {
+        // Press configured modifiers
+        if (generalconfig.modifier1 != 0) {
+          bleCombo.keyPress(generalconfig.modifier1);
+        }
+        if (generalconfig.modifier2 != 0) {
+          bleCombo.keyPress(generalconfig.modifier2);
+        }
+        if (generalconfig.modifier3 != 0) {
+          bleCombo.keyPress(generalconfig.modifier3);
+        }
+        
+        // Press the function key
+        bleCombo.keyPress(helperFunctionKeys[value]);
+        bleCombo.keyReleaseAll();
+        delay(generalconfig.helperdelay);
       }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F1);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
-    case 2:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
-      }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F2);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
-    case 3:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
-      }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F3);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
-    case 4:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
-      }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F4);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
-    case 5:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
-      }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F5);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
-    case 6:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
-      }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F6);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
-    case 7:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
-      }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F7);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
-    case 8:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
-      }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F8);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
-    case 9:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
-      }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F9);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
-    case 10:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
-      }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F10);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
-    case 11:
-      if (generalconfig.modifier1 != 0) {
-        bleCombo.keyPress(generalconfig.modifier1);
-      }
-      if (generalconfig.modifier2 != 0) {
-        bleCombo.keyPress(generalconfig.modifier2);
-      }
-      if (generalconfig.modifier3 != 0) {
-        bleCombo.keyPress(generalconfig.modifier3);
-      }
-      bleCombo.keyPress(KEY_F11);
-      bleCombo.keyReleaseAll();
-      delay(generalconfig.helperdelay);
-      break;
     }
     break;
   case 11: // Special functions
@@ -487,55 +240,29 @@ void bleKeyboardAction(int action, int value, char *symbol) {
     }
     break;
   case 12: // Numpad
-    switch (value) {
-    case 0:
-      bleCombo.write(KEY_NUM_0);
-      break;
-    case 1:
-      bleCombo.write(KEY_NUM_1);
-      break;
-    case 2:
-      bleCombo.write(KEY_NUM_2);
-      break;
-    case 3:
-      bleCombo.write(KEY_NUM_3);
-      break;
-    case 4:
-      bleCombo.write(KEY_NUM_4);
-      break;
-    case 5:
-      bleCombo.write(KEY_NUM_5);
-      break;
-    case 6:
-      bleCombo.write(KEY_NUM_6);
-      break;
-    case 7:
-      bleCombo.write(KEY_NUM_7);
-      break;
-    case 8:
-      bleCombo.write(KEY_NUM_8);
-      break;
-    case 9:
-      bleCombo.write(KEY_NUM_9);
-      break;
-    case 10:
-      bleCombo.write(KEY_NUM_SLASH);
-      break;
-    case 11:
-      bleCombo.write(KEY_NUM_ASTERISK);
-      break;
-    case 12:
-      bleCombo.write(KEY_NUM_MINUS);
-      break;
-    case 13:
-      bleCombo.write(KEY_NUM_PLUS);
-      break;
-    case 14:
-      bleCombo.write(KEY_NUM_ENTER);
-      break;
-    case 15:
-      bleCombo.write(KEY_NUM_PERIOD);
-      break;
+    {
+      static const uint8_t numpadKeyMap[] = {
+        KEY_NUM_0,        // 0
+        KEY_NUM_1,        // 1
+        KEY_NUM_2,        // 2
+        KEY_NUM_3,        // 3
+        KEY_NUM_4,        // 4
+        KEY_NUM_5,        // 5
+        KEY_NUM_6,        // 6
+        KEY_NUM_7,        // 7
+        KEY_NUM_8,        // 8
+        KEY_NUM_9,        // 9
+        KEY_NUM_SLASH,    // 10
+        KEY_NUM_ASTERISK, // 11
+        KEY_NUM_MINUS,    // 12
+        KEY_NUM_PLUS,     // 13
+        KEY_NUM_ENTER,    // 14
+        KEY_NUM_PERIOD    // 15
+      };
+      
+      if (value >= 0 && value < sizeof(numpadKeyMap)/sizeof(numpadKeyMap[0])) {
+        bleCombo.write(numpadKeyMap[value]);
+      }
     }
     break;
   case 13: // Custom functions
